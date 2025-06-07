3cb7e0e94bc5a39f52b02fc1d076b82c4da70088
AvindiNavodya <avindinavodya@gmail.com>
2025-06-08 01:45:55 +0530

    splash and login screens were created and linked

--- app/src/main/java/com/example/mobile_application_assignment02/loginscreen.java
+++ app/src/main/java/com/example/mobile_application_assignment02/loginscreen.java
@@ -1,8 +1,6 @@
 package com.example.mobile_application_assignment02;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final int SPLASH_DELAY = 2000; // 2 seconds
+public class loginscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_loginscreen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Handler to move to login screen after delay
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(MainActivity.this, loginscreen.class);
-            startActivity(intent);
-            finish(); // Close splash so user can't go back
-        }, SPLASH_DELAY);
     }
 }
