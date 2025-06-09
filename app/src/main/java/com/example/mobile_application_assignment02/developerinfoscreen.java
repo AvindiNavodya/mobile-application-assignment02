0d1c094795d849278ca25c30285eba6579210482
AvindiNavodya <avindinavodya@gmail.com>
2025-06-09 23:11:51 +0530

    linked developer information screen and news screen

--- app/src/main/java/com/example/mobile_application_assignment02/developerinfoscreen.java
+++ app/src/main/java/com/example/mobile_application_assignment02/developerinfoscreen.java
@@ -1,6 +1,8 @@
 package com.example.mobile_application_assignment02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,19 @@ public class developerinfoscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_developerinfoscreen);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // exit to News Screen when EXIT button is clicked
+        Button exitButton = findViewById(R.id.button); // EXIT button ID
+        exitButton.setOnClickListener(v -> {
+            Intent intent = new Intent(developerinfoscreen.this, newsscreen.class);
+            startActivity(intent);
+            finish(); // Optional: clear this screen from back stack
         });
     }
 }
--- app/src/main/java/com/example/mobile_application_assignment02/newsscreen.java
+++ app/src/main/java/com/example/mobile_application_assignment02/newsscreen.java
@@ -1,6 +1,8 @@
 package com.example.mobile_application_assignment02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,18 @@ public class newsscreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_newsscreen);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //  Navigate to Developer Info screen when error icon is clicked
+        ImageView errorIcon = findViewById(R.id.imageView8); // Error icon ID
+        errorIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(newsscreen.this, developerinfoscreen.class);
+            startActivity(intent);
         });
     }
 }
