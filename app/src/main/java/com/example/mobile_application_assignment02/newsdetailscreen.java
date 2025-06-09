594eaca6c5f0a08bfa65489833c39bf22a232af3
AvindiNavodya <avindinavodya@gmail.com>
2025-06-10 01:25:29 +0530

    news detail screen added

---
+++ app/src/main/java/com/example/mobile_application_assignment02/newsdetailscreen.java
@@ -0,0 +1,24 @@
+package com.example.mobile_application_assignment02;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class newsdetailscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_newsdetailscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
